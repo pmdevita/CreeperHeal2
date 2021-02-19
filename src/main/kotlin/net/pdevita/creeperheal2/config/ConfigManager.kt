1b5419a2e44547cbb6a35e2a26e72fad7475c922
Peter DeVita <mewtwo2643@yahoo.com>
2021-02-19 15:42:52 -0500

    Added coercion to prevent dangerously high turbo cycles

--- src/main/kotlin/net/pdevita/creeperheal2/config/ConfigManager.kt
+++ src/main/kotlin/net/pdevita/creeperheal2/config/ConfigManager.kt
@@ -17,9 +17,9 @@ class General(config: FileConfiguration) {
     val explodeTNT = config.getBoolean("explode-tnt", true)
     val turboThreshold = config.getInt("turbo-threshold", 5000)
     val turboType = config.getInt("turbo-type", 0).coerceIn(0..1)
-    val turboAmount = config.getInt("turbo-amount", 3)
+    val turboAmount = config.getInt("turbo-amount", 3).coerceAtMost(1000)
     val turboPercentage = config.getInt("turbo-percentage", 1).coerceIn(1..100)
-    val turboCap = config.getInt("turbo-cap", 10)
+    val turboCap = config.getInt("turbo-cap", 10).coerceAtMost(1000)
 }
 
 class ExplosionTypes(config: FileConfiguration) {
