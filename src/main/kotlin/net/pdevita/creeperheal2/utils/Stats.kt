310734f951e391b5356d02e3ac2ecc6a1c2ebd89
Peter DeVita <mewtwo2643@yahoo.com>
2022-12-27 17:15:19 -0500

    Update Bstats to 3.0.0

--- src/main/kotlin/net/pdevita/creeperheal2/utils/Stats.kt
+++ src/main/kotlin/net/pdevita/creeperheal2/utils/Stats.kt
@@ -2,6 +2,7 @@ package net.pdevita.creeperheal2.utils
 
 import net.pdevita.creeperheal2.CreeperHeal2
 import org.bstats.bukkit.Metrics
+import org.bstats.charts.SingleLineChart
 import java.util.concurrent.Callable
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -11,10 +12,10 @@ class Stats(private val plugin: CreeperHeal2) {
     val totalBlocks = AtomicInteger(0)
 
     init {
-        metrics.addCustomChart(Metrics.SingleLineChart("explosions_restored_over_time", Callable {
+        metrics.addCustomChart(SingleLineChart("explosions_restored_over_time", Callable {
             return@Callable totalExplosions.getAndSet(0)
         }))
-        metrics.addCustomChart(Metrics.SingleLineChart("blocks_replaced", Callable {
+        metrics.addCustomChart(SingleLineChart("blocks_replaced", Callable {
             return@Callable totalBlocks.getAndSet(0)
         }))
     }
