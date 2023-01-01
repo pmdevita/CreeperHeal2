a46586844ca9840ea85fcc078aad58e3b1bbc7e3
Peter DeVita <mewtwo2643@yahoo.com>
2022-12-31 20:21:59 -0500

    Revert "Add extra debugging for Movecraft"
    This reverts commit 75563404bcbdb91e130c051938b227f6f60c46ee.

--- src/main/kotlin/net/pdevita/creeperheal2/compatibility/Movecraft.kt
+++ src/main/kotlin/net/pdevita/creeperheal2/compatibility/Movecraft.kt
@@ -3,7 +3,6 @@ package net.pdevita.creeperheal2.compatibility
 import com.google.auto.service.AutoService
 import net.countercraft.movecraft.craft.CraftManager
 import net.countercraft.movecraft.util.MathUtils.locationNearHitBox
-import net.pdevita.creeperheal2.CreeperHeal2
 import net.pdevita.creeperheal2.core.Boundary
 import org.bukkit.Location
 import org.bukkit.World
@@ -11,30 +10,22 @@ import org.bukkit.block.Block
 
 object Movecraft : BaseCompatibility {
     override val pluginName = "Movecraft"
-    lateinit var creeperHeal: CreeperHeal2
-
-    override fun setCreeperHealReference(creeperHeal2: CreeperHeal2) {
-        creeperHeal = creeperHeal2
-    }
-
     override fun maskBlocksFromExplosion(blockList: MutableList<Block>, world: World, boundary: Boundary, center: Location) {
-        var counter = 0
-        this.creeperHeal.debugLogger("Checking Movecraft for explosion at $center")
+//        var counter = 0
         for (craft in CraftManager.getInstance().getCraftsInWorld(world)) {
-            this.creeperHeal.debugLogger("Checking against craft ${craft.name} at ${craft.hitBox.midPoint}")
             if (locationNearHitBox(craft.hitBox, center, 50.0)) {
-                this.creeperHeal.debugLogger("Found craft ${craft.name} near explosion, masking blocks")
+                println("Found craft ${craft.name} near explosion, masking blocks")
                 val itr = blockList.iterator()
                 while (itr.hasNext()) {
                     val block = itr.next()
                     if (craft.hitBox.contains(block.x, block.y, block.z)) {
                         itr.remove()
-                        counter++
+//                        counter++
                     }
                 }
             }
         }
-        this.creeperHeal.debugLogger("Masked out $counter blocks from the explosion")
+//        println("Masked out $counter blocks from the explosion")
     }
 }
 
