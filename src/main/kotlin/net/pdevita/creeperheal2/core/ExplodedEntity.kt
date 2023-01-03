89796f8f7d29c0f3dead751342516032faddba13
Peter DeVita <mewtwo2643@yahoo.com>
2023-01-03 17:59:02 -0500

    Add algorithm for Painting movement vector, add GlowItemFrame support

--- src/main/kotlin/net/pdevita/creeperheal2/core/ExplodedEntity.kt
+++ src/main/kotlin/net/pdevita/creeperheal2/core/ExplodedEntity.kt
@@ -2,6 +2,7 @@ package net.pdevita.creeperheal2.core
 
 import org.bukkit.Art
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.Rotation
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.*
@@ -9,7 +10,7 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.util.Vector
 
 open class ExplodedEntity() {
-    open var entityType: EntityType? = null
+    open val entityType: EntityType? = null
     lateinit var location: Location
 
     companion object {
@@ -17,6 +18,7 @@ open class ExplodedEntity() {
             return when (entity) {
                 is org.bukkit.entity.Painting -> ExplodedPainting(entity)
                 is org.bukkit.entity.ItemFrame -> ExplodedItemFrame(entity)
+                if org.bukkit.entity.GlowItemFrame -> ExplodedGlowItemFrame(entity)
                 else -> throw java.lang.Exception("$entity is not a supported exploded entity")
             }
         }
@@ -32,7 +34,14 @@ open class ExplodedEntity() {
     }
 
     fun spawnEntity(location: Location): Entity? {
-        val entityClass = entityType!!.entityClass!!
+        val entityClass = entityType?.entityClass!!
+        val currentBlock = location.block
+
+        // If block isn't air, it's likely a player put it there. Just break it off normally to give it back to them
+        if (currentBlock.blockData.material != Material.AIR) {
+            currentBlock.breakNaturally()
+        }
+
         return location.world?.spawn(location, entityClass) { loadData(it) }
     }
 
@@ -69,77 +78,43 @@ open class ExplodedHanging(entity: Entity): ExplodedEntity(entity) {
 }
 
 class ExplodedPainting(entity: Entity) : ExplodedHanging(entity) {
-    override var entityType: EntityType? = EntityType.PAINTING
+    override val entityType: EntityType = EntityType.PAINTING
     private lateinit var art: Art
-
-    companion object {
-        val data = HashMap<BlockFace, HashMap<String, Vector>>()
-
-        fun addPaintingData(direction: BlockFace, size: String, offset: Vector) {
-            if (!data.contains(direction)) {
-                data[direction] = HashMap()
-            }
-            val directionMap = data[direction]!!
-            if (!directionMap.contains(size)) {
-                directionMap[size] = offset
-            } else {
-                if (directionMap[size] != offset) {
-                    println("Different offsets!!! $direction $size ${directionMap[size]} != ${offset}")
-                }
-            }
-        }
-
-        fun printPaintingData() {
-            println("Painting Stats:")
-            for (direction in data.keys) {
-                val directionMap = data[direction]!!
-                println("$direction:")
-                for (size in directionMap.keys) {
-                    println("$size - ${directionMap[size]}")
-                }
-            }
-        }
-
-    }
 
     override fun saveData(entity: Entity) {
         super.saveData(entity)
-        val painting = entity as org.bukkit.entity.Painting
+        val painting = entity as Painting
         art = painting.art
+        location.subtract(getPaintingOffset(facingDirection, art))
+    }
+
+    // Painting data probably kept somewhere
+    private fun getPaintingOffset(direction: BlockFace, art: Art): Vector {
+        val vector = Vector(0, 0, 0)
+        if (art.blockHeight % 2 == 0) {
+            vector.y = 1.0
+        }
+        if (art.blockWidth >= 2) {
+            if (direction == BlockFace.SOUTH) {
+                vector.x = 1.0
+            } else if (direction == BlockFace.WEST) {
+                vector.z = 1.0
+            }
+        }
+        return vector
     }
 
     override fun loadData(entity: Entity) {
         // Use the normally spawned painting to figure out how far
         // it will move after we set the art
         super.loadData(entity)
-        val painting = entity as org.bukkit.entity.Painting
+        val painting = entity as Painting
         painting.setArt(art, true)
-        val movedLocation = painting.location
-        val moveX = movedLocation.x - location.x
-        val moveY = movedLocation.y - location.y
-        val moveZ = movedLocation.z - location.z
-        entity.remove()
-        // Correct the location and make a new painting
-        val moveVector = Vector(moveX, moveY, moveZ)
-        addPaintingData(facingDirection, "${art.blockWidth}x${art.blockHeight}", moveVector)
-        val fixedLocation = location.clone()
-        fixedLocation.subtract(moveVector)
-        val entityClass = entityType!!.entityClass!!
-        location.world?.spawn(fixedLocation, entityClass) {
-            super.loadData(it)
-            val newPainting = it as Painting
-            newPainting.setArt(art, true)
-
-        }
-        println("Painting of size ${art.blockWidth}x${art.blockHeight} was originally at $location. " +
-                "After placing and setting art, it moved by $moveVector. It was then deleted and placed at " +
-                "$fixedLocation.")
-        printPaintingData()
     }
 }
 
-class ExplodedItemFrame(entity: Entity) : ExplodedHanging(entity) {
-    override var entityType: EntityType? = EntityType.ITEM_FRAME
+open class ExplodedItemFrame(entity: Entity) : ExplodedHanging(entity) {
+    override val entityType: EntityType? = EntityType.ITEM_FRAME
     private lateinit var itemStack: ItemStack
     private lateinit var rotation: Rotation
 
@@ -158,3 +133,6 @@ class ExplodedItemFrame(entity: Entity) : ExplodedHanging(entity) {
     }
 }
 
+class ExplodedGlowItemFrame(entity: Entity): ExplodedItemFrame(entity) {
+    override val entityType: EntityType = EntityType.GLOW_ITEM_FRAME
+}
